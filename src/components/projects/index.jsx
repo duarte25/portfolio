131506182a27ad0de390f992db8d50884733c023
duarte25 <duarte.guga2025@gmail.com>
2024-07-29 18:15:25 -0400

    Arrumando os portfolios

--- src/components/projects/index.jsx
+++ src/components/projects/index.jsx
@@ -1,5 +1,7 @@
+/* eslint-disable react/no-unescaped-entities */
 import styles from "./styles.module.css"
 import { CgWebsite } from "react-icons/cg";
+import { FiFigma } from "react-icons/fi";
 import { BottomNavigation, BottomNavigationAction, Card, CardContent, CardMedia, Typography } from "@mui/material";
 import * as React from 'react';
 
@@ -51,6 +53,30 @@ export default function Perfil() {
                     </CardContent>
                     <BottomNavigation className={styles.bottomNavigation}>
                         <BottomNavigationAction label="Web" icon={<CgWebsite />} className={styles.icon} />
+                        <BottomNavigationAction label="Web" icon={<FiFigma />} className={styles.icon} />
+                    </BottomNavigation>
+                </Card>
+
+                <Card className={styles.card}>
+                    <CardMedia
+                        className={styles.cardMediaSeuUsado}
+                        component="img"
+                        image="/seuusado.png"
+                        alt="Cidades Inteligentes"
+                    />
+                    <CardContent className={styles.legendCard}>
+                        <Typography gutterBottom variant="h6" component="div">
+                            Seuusado.com
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            This impressive paella is a perfect party dish and a fun meal to cook
+                            together with your guests. Add 1 cup of frozen peas along with the mussels,
+                            if you like.
+                        </Typography>
+                    </CardContent>
+                    <BottomNavigation className={styles.bottomNavigation}>
+                        <BottomNavigationAction label="Web" icon={<CgWebsite />} className={styles.icon} />
+                        <BottomNavigationAction label="Web" icon={<FiFigma />} className={styles.icon} />
                     </BottomNavigation>
                 </Card>
             </div>
