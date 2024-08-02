86659f48cbf00b87736fb4315dae22dd619695f3
duarte25 <duarte.guga2025@gmail.com>
2024-08-02 16:53:38 -0400

    Tornando parte de perfil responsiva

--- src/components/perfil/index.jsx
+++ src/components/perfil/index.jsx
@@ -7,20 +7,15 @@ export default function Perfil() {
         <div className={styles.perfil}>
             <div className={styles.blockLeft}>
                 <h2>𝕲𝖚𝖘𝖙𝖆𝖛𝖔</h2>
-
                 <div className={styles.description}>
-
                     <h1>Desenvolvedor</h1>
                     <h3>Viso transformar ideias inovadoras em soluções tecnológicas que simplifiquem a vida das pessoas.</h3>
-
                     <div className={styles.topicPerfil}>
                         <h4>Experiência abrangente em design UI/UX e no desenvolvimento completo de páginas web, desde a concepção inicial até a implementação final.</h4>
                         <h4>Especializado em desenvolvimento back-end, com foco em criar soluções que atendem às necessidades dos clientes.</h4>
                     </div>
-
                 </div>
             </div>
-
             <div className={styles.blockRight}>
                 <div className={styles.containerPerfil}>
                     <div className={styles.avatarWrapper}>
