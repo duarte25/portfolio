84b4f989b180ee84d98705fb20416c849c68ea36
Gustavo Vinicius Duarte Da Silva <duarte.guga2025@gmail.com>
2025-08-23 15:51:44 -0400

    fix: corrigindo bug

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -3,10 +3,10 @@ import HomePage from "@/react-app/pages/Home";
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </Router>
+
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+
   );
 }
