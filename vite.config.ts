9f49eb4ece1e049eb0a2acaa79b6319d670c804d
Gustavo Vinicius Duarte Da Silva <duarte.guga2025@gmail.com>
2025-08-23 15:31:34 -0400

    fix: Corrigindo deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 import { mochaPlugins } from "@getmocha/vite-plugins";
 
 export default defineConfig({
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  base: '/portfolio/',
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
   plugins: [...mochaPlugins(process.env as any), react(), cloudflare()],
   server: {
     allowedHosts: true,
