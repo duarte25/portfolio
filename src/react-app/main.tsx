eee5b70a88a3fcd3057e52e118014e300cbe4891
Gustavo Vinicius Duarte Da Silva <duarte.guga2025@gmail.com>
2025-08-23 15:46:56 -0400

    fix: corrigindo bug

--- src/react-app/main.tsx
+++ src/react-app/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // 1. Importar o BrowserRouter
 import "@/react-app/index.css";
 import App from "@/react-app/App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    {/* 2. Envolver o App com o BrowserRouter e passar a base URL */}
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
